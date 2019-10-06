9b2e04a2e70739d25ecef211e6a8d83be4e64cd3
system3-jgp <56217183+system3-jgp@users.noreply.github.com>
2019-10-06 11:02:39 +0300

    Add files via upload

---
+++ source/module.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+
+exports.run = function(client, message)  {
+	//Command codes
+}
+exports.conf = {
+	enabled:true,
+	guildOnly:false,
+	aliases:[],
+	permLevel:0
+}
+exports.help = {
+	name:"",//Command name
+	description:"",//Commmand description
+	usage:"",//Command usage
+}
---
+++ source/video.js
@@ -0,0 +1,18 @@
+//Başlamadan önce şunu diyeceğim. Aslında bu otorol komutu quick.db ile yapılıyo
+/*
+
+Ancak quick.db windowsta kurlmayan bir modül. 
+Biz JSON ile verileri depolayacağız
+JSON'dan çekeceğiz
+JSON windows platformunda desteklenmekle beraber dosya silinmediği sürece 
+veriler kalıcı olur
+Evet hazırsanız videoya geçelim
+*/
+
+//Evet bu videoda bir arkadaşın isteği üzerine küfür engel kodlayacağız
+/*
+Yeniden JSOn ile yapıcaz
+Sebebi yukarda yazıyor
+Pazartesi günü olmayabilirim ve daha sonra girmeyebiliirm ara sıra girebilirim
+Neyse videoya geçelim
+*/
